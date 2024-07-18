6b36898d7bc9da28cc864d9c79669913156e07ea
sameerExpansionjs <sameer@expansionjs.com>
2024-07-18 13:25:11 +0530

    locomotive js

--- script.js
+++ script.js
@@ -26,36 +26,35 @@
 //   scrollTrigger: "#page3 #circle",
 // });
 
-gsap.to("#page2 img", {
-  width: "100%",
-  scrollTrigger: {
-    trigger: "#page2",
-    scroller: "body",
-    markers: true,
-    start: "top 0",
-    end: "top -100%",
-    scrub: 3,
-    pin: true,
-  },
-});
+// gsap.to("#page2 img", {
+//   width: "100%",
+//   scrollTrigger: {
+//     trigger: "#page2",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 0",
+//     end: "top -100%",
+//     scrub: 3,
+//     pin: true,
+//   },
+// });
 
-gsap.to("#heading", {
-  transform: "translateX(-550%)",
-  scrollTrigger: {
-    // trigger: "#page4",
-    // scroller: "body",
-    // markers: true,
-    // start: "top 0",
-    // end: "top -100%",
-    // scrub: 2,
-    // pin: true,
+// gsap.to("#heading", {
+//   transform: "translateX(-550%)",
+//   scrollTrigger: {
+//     trigger: "#page4",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 0",
+//     end: "top -100%",
+//     scrub: 2,
+//     pin: true,
+//   },
+// });
 
-    trigger: "#page4",
-    scroller: "body",
-    markers: true,
-    start: "top 0",
-    end: "top -100%",
-    scrub: 2,
-    pin: true,
-  },
+// Locomotive
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
 });
